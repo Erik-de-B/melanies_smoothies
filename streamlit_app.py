c37ecb8593d7ab23a8de8317c4d438eaf4f8aca7
Erik-de-B <124250784+Erik-de-B@users.noreply.github.com>
2026-07-20 14:58:23 +0100

    Disable Snowflake connection in streamlit_app.pytesting streamlit/github connection
    Comment out Snowflake connection and data retrieval.
    
    cnx = st.connection("snowflake")
    session = cnx.session()
    my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
    
    added: st.write("Hello from Streamlit Cloud!")

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,11 @@ st.write(
 name_on_order = st.text_input ('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-cnx = st.connection("snowflake")
-session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#cnx = st.connection("snowflake")
+#session = cnx.session()
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+st.write("Hello, Is there anybody out there?")
+
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
